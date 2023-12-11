7e120e10a5a2c0038138b765c8811d7242c72ed0
code malayalam <danaya@example.com>
2023-12-11 21:41:02 +0530

    landing still editing

--- src/pages/home.js
+++ src/pages/home.js
@@ -57,7 +57,6 @@ class Home  extends React.Component{
                                     </div>
                                 </div>
             
-                               
                                 <div class="site-navigation">
                                     <div class="hamburger-menus">
                                         <span></span>
